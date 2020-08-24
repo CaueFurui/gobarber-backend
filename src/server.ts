db9378c51ab1c93ddd8e0633dec33dbccff22531
CaueFurui <cauefurui@gmail.com>
2020-08-24 15:48:42 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import routes from './routes'
+
+const app = express()
+app.use(express.json())
+const PORT = 3333
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}...`)
+})
