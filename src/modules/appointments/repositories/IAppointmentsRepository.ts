b5c8e7878f959fdc199aa8aae6ac4557082e92bb
CaueFurui <caue.martins@sambatech.com.br>
2020-12-15 22:07:57 -0300

    [PROVIDERS]Listing unavailable days

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthProviderDTO';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentsDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
