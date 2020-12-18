0cf31af599546b6f1106e291baf51838d0859e42
CaueFurui <caue.martins@sambatech.com.br>
2020-12-18 13:13:37 -0300

    Adding list the provider appointments

--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -4,7 +4,8 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { provider_id, month, year } = request.body;
+    const { month, year } = request.body;
+    const { provider_id } = request.params;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailabilityService,
